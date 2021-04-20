d3740a83418fc33262e13ff35034a0566a57b9a5
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2021-04-20 21:34:45 +0600

    Create p6.c

---
+++ Recursion/Some solutions/p6.c
@@ -0,0 +1,34 @@
+//Problem 6
+//Print reverse string 
+
+#include <stdio.h>
+
+void reverseSentence(); 
+
+int main()
+
+ {
+
+     reverseSentence(); 
+
+      return 0; 
+
+ } 
+
+void reverseSentence() 
+
+ { 
+
+    char c;
+
+    scanf("%c", &c); 
+
+    if (c !='\n') //if the variable isn't new line character, recursive function calls again.
+
+    { 
+
+        reverseSentence(); 
+
+        printf("%c",c);//when the variable reaches of it's new line character,then it will be printed the last value and as we were go on.
+
+    } 
